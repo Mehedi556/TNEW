074d4aa818a2a0e3804e5c7a390303d6262f4ea6
mehedi556 <mehe556di@gmail.com>
2022-09-04 09:44:42 +0600

    adding single categories function

--- js/app.js
+++ js/app.js
@@ -24,3 +24,55 @@ const setAllCategories = async() => {
 
     }
 }
+
+
+
+const categoryDetail = (code) => {
+    // console.log(code);
+    const loadDetail = ` https://openapi.programming-hero.com/api/news/category/${code}`;
+
+fetch(loadDetail)
+.then(res => res.json())
+.then(data => displaySingleCategories(data.data))
+    
+}
+
+const displaySingleCategories = (items) => {
+    // console.log(items);
+
+    const singleCategori = document.getElementById('single-category');
+    singleCategori.innerHTML = '';
+    for(const item of items){
+        // console.log(item);
+
+const {thumbnail_url , title , total_view , details , image_url , author , _id} = item;
+
+const creatingDiv = document.createElement('div');
+creatingDiv.classList.add = ('rounded-md');
+creatingDiv.innerHTML = `
+
+<figure class="w-3/3 mx-auto"><img src="${image_url}" alt="Album"></figure>
+    <div class="card-body bg-slate-700">
+      <h2 class="card-title">${title}</h2>
+      <p>${details.slice(0 , 200)}</p>
+      <p><i class="fa-solid fa-arrows-to-eye"></i>: ${total_view ? total_view : "No viewers found"}</p>
+      <div class="d-flex">
+      <img class="author-image" src="${author.img}" alt="">
+      <div>
+      <p>${author.name ? author.name : "No name found"}</p>
+      <p>${author.published_date ? author.published_date : "No date found"}</p>
+      </div>
+      
+      </div>
+      
+      <div class="card-actions justify-end">
+        
+      <label onclick="modalDetailBtn('${_id}')" for="my-modal-4" class="btn modal-button">Details</label>
+        
+      </div>
+    </div>
+
+`;
+singleCategori.appendChild(creatingDiv);
+}
+}
