c7d479590f1d8ff5347d1972b592a1cf4264359c
mehedi556 <mehe556di@gmail.com>
2022-09-04 09:21:28 +0600

    adding categories function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ const loadAllCategories = async() => {
     // console.log(data);
     return data.data.news_category;
 }
+
+const setAllCategories = async() => {
+    const data = await loadAllCategories();
+    // console.log(data);
+
+    const ul = document.getElementById('category') 
+
+    for(const category of data){
+        // console.log(category.category_id);
+
+        const li = document.createElement('li');
+        
+        li.innerHTML = `
+        <a onclick = "categoryDetail('${category.category_id}')">${category.category_name}</a>
+        `;
+
+        ul.appendChild(li);
+
+    }
+}
